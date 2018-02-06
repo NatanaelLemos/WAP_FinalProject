eab701ee6bc9f8ddb67840b7e1ec11821cf050ef
MSI\sadok <sadok.chebil@gmail.com>
2018-02-06 18:56:33 +0100

    Login + Authentification

--- src/main/java/web/controllers/LoginServlet.java
+++ src/main/java/web/controllers/LoginServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/login")
@@ -12,5 +13,12 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/jsp/login.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.setAttribute("userName","sadok");
+        resp.sendRedirect("/home");
     }
 }
