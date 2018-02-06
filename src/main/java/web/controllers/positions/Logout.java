a3ddde51913ecb4e40e5aa53fb286cc49799b367
MSI\sadok <sadok.chebil@gmail.com>
2018-02-06 18:55:24 +0100

    Logout Servlet

---
+++ src/main/java/web/controllers/positions/Logout.java
@@ -0,0 +1,26 @@
+package web.controllers.positions;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by sadok on 06/02/2018.
+ */
+
+@WebServlet("/logout")
+public class Logout extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if(session!= null) {
+            session.invalidate();
+        }
+        resp.sendRedirect("/login");
+    }
+}
